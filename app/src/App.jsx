e9c1155ededf7adbd31193bb38a256edb5101d47
Mike <mt.mail.mt@gmail.com>
2024-05-03 14:08:20 +0300

    second commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,24 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import '@chatscope/chat-ui-kit-react-styles/dist/default/styles.min.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
 
+
 function App() {
-  const [message, setMessage] = useState([
+
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
     {
       message: "Hello, I am ChatGPT!",
-      sender: "ChatGPT"
+      sender: "ChatGPT",
+      direction: "incoming"
     }
   ])
 
   const handleSend = async (message) => {
     const newMessage = {
       message: message,
-      sender: "user"
+      sender: "user",
+      direction: "outgoing"
     }
 
-    const newMessages = [...messages, newMessage];
+    const newMessages = [...messages, newMessage]; //all the old messages, + the new message
+
+    // update our messages state
+    setMessages(newMessages);
+
+    //set a typing indicator (chatgpt is typing)
+    setTyping(true);
+
+    // process message to chatGPT (sent it over and see the response)
+    await processMessageToChatGPT(newMessages)
+  }
+
+  async function processMessageToChatGPT(chatMessages) {
+
   }
 
   return (
@@ -27,8 +45,10 @@ function App() {
       <div style={{ position: "relative", height: "800px", width: "700px"}}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
-              {message.map((message) => {
+            <MessageList
+              typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing" /> : null}
+            >
+              {messages.map((message, i) => {
                 return <Message key={i} model={message} />
               })}
             </MessageList>
