408d35b046bc67017b2d4846a3ce1b56bb279b3a
Mike <mt.mail.mt@gmail.com>
2024-05-03 15:34:08 +0300

    Third commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,7 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const API_KEY = process.env.API_KEY;
 
 function App() {
 
@@ -36,7 +40,55 @@ function App() {
     await processMessageToChatGPT(newMessages)
   }
 
-  async function processMessageToChatGPT(chatMessages) {
+  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+    // Format messages for chatGPT API
+    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
+    // So we need to reformat
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if(messageObject.sender === "ChatGPT") {
+        role="assistant"
+      } else {
+        role = "user"
+      }
+      return { role : role, content: messageObject.message }
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: "Speak as if you were a pedagogue teacher."
+    }
+
+    const apiRequestBody = {
+      "model" : "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessages
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      console.log(data.choices[0].message.content);
+      setMessages(
+        [...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT",
+          direction: "incoming"
+        }]
+      );
+      setTyping(false);
+    })
 
   }
 
@@ -46,6 +98,7 @@ function App() {
         <MainContainer>
           <ChatContainer>
             <MessageList
+              scrollBehavior='smooth'
               typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing" /> : null}
             >
               {messages.map((message, i) => {
